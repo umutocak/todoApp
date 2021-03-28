19b7bd9721e38f36a41a46bfca32b7290bd45057
Umut Ocak <48753655+umutocak@users.noreply.github.com>
2021-03-28 11:36:30 +0300

    Lesson 3: Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'Umut Ocak';
+  items= [
+    { description: "Kahvaltı",action:"No"},
+    { description: "Sinema",action:"No"},
+    { description: "Spor",action:"No"},
+    { description: "Ödev yap",action:"No"}
+  ];
 }
